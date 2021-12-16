233f429b3beeab983e15e623c2d1df97de0ae554
Fernando Baeza Hurtado <91624660+ferbaeza@users.noreply.github.com>
2021-12-17 00:19:44 +0100

    mongobd app

--- mongoDB_acti/models/player.js
+++ mongoDB_acti/models/player.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const playerSchema = new mongoose.Schema({
     name: {
         type: String,
-        required : false,
+        required : false
     },
     surname: {
         type: String,
